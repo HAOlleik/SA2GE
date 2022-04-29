2bb5399622f53db57c561d83c9cf1c9292b761a5
neil3524 <neil3524@gmail.com>
2022-04-29 01:12:02 -0400

    Add example of client side fetching

--- sa2ge-example/pages/api/hello.js
+++ sa2ge-example/pages/api/hello.js
@@ -1,10 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 /**
  *
  * @param {import("next").NextApiRequest} req
  * @param {import("next").NextApiResponse} res
  */
-export default function handler(req, res) {
-  res.status(200).json({ name: "John Doe" });
+export default async function handler(req, res) {
+  // add 1s delay to simulate a server call
+  await sleep(1000);
+  // send the response
+  // res.status(200).json({ name: "John Doe" });
+  // or
+  res.send({ name: "John Doe" });
 }
--- sa2ge-example/pages/index.js
+++ sa2ge-example/pages/index.js
@@ -2,14 +2,40 @@ import styles from "../styles/Home.module.css";
 
 import { useTranslation } from "next-i18next";
 import { serverSideTranslations } from "next-i18next/serverSideTranslations";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { t } = useTranslation();
+  const [name, setName] = useState();
+
+  const nameApi = "/api/hello";
+
+  // use a useEffect hook to fetch the name from the server
+  // should always be used when changing something on the ui
+  // only runs after the first render
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(nameApi);
+      const data = await response.json();
+      // setState rerenders the whole component
+      setName(data.name);
+    };
+    fetchData();
+    // add empty dependency array to call this only once on first render
+  }, []);
 
   return (
     <div className={styles.container}>
       <h1>{t("title")}</h1>
       <h3>{t("description")}</h3>
+      {name ? (
+        <p>
+          Fetched Name from <a href={nameApi}>{nameApi}</a>:{" "}
+          <span style={{ color: "limegreen" }}>{name}</span>
+        </p>
+      ) : (
+        <p>{t("loading")}</p>
+      )}
     </div>
   );
 }
